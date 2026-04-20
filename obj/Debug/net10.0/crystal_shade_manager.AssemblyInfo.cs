4738b81f98685f3314879e175c2db3a7ae4fe9ec
Bodianchis <8iiiii5556@gmail.com>
2026-04-20 18:13:06 +0300

    update 20.04

--- obj/Debug/net10.0/crystal_shade_manager.AssemblyInfo.cs
+++ obj/Debug/net10.0/crystal_shade_manager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("crystal_shade_manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fadb462cab04f6243a2ca35f57a3fb2ea4474776")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ca7bf759e1917893f58d1ad399fa46956ae1762")]
 [assembly: System.Reflection.AssemblyProductAttribute("crystal_shade_manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("crystal_shade_manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
